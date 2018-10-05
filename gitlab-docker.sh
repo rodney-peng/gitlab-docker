b2fdf7a570ffa875862c818edc061ed8e15589a9
Rodney Peng <rodney_peng@alphanetworks.com>
2018-10-05 20:06:22 +0000

    add certificates for 5.98.17.201

--- gitlab-docker.sh
+++ gitlab-docker.sh
@@ -1,18 +1,18 @@
 # run gitlab in docker container before Docker Compose can support user namespace
 
-docker rm --force gitlab
-docker pull gitlab/gitlab-ce
+#docker rm --force gitlab
+#docker pull gitlab/gitlab-ce
 
 docker run --detach \
 --hostname gitlab \
 -p 80:80 -p 443:443 -p 722:22 -p 5005:5005 \
 --name gitlab \
 --restart always \
--v /mnt/data/gitlab/config:/etc/gitlab \
--v /mnt/data/gitlab/logs:/var/log/gitlab \
--v /mnt/data/gitlab/data:/var/opt/gitlab \
-gitlab/gitlab-ce
+-v /home/gitlab/config:/etc/gitlab \
+-v /home/gitlab/logs:/var/log/gitlab \
+-v /home/gitlab/data:/var/opt/gitlab \
+polinux/gitlab-ce:10.0.4-ce.0
 
-sleep 15
+#sleep 15
 #docker exec gitlab chown root:root /run/sshd
-docker exec -it gitlab bash -c "apt update && apt install -y postfix && service postfix start"
+#docker exec -it gitlab bash -c "apt update && apt install -y postfix && service postfix start"
